796ef5087300732137314aac72cadfbd9e418009
zbednarz <zbigniew.bednarz@icloud.com>
2022-08-16 19:48:32 +0200

    Changes made after first review

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,48 +1,42 @@
-import React from "react";
+import uuid from "react-uuid";
 
-const Form =({input, setInput, todos, setTodos, setFilter})=>{
+const Form = ({ input, setInput, todos, setTodos, setFilter }) => {
+  const inputHandler = (e) => {
+    setInput(e.target.value);
+  };
 
-    const inputHandler  = (e) =>{
-       console.log(e.target.value);
-        setInput(e.target.value);
-    }
-    
-    const formSubmit = (e) => {
-        e.preventDefault()
-        setTodos([
-                ...todos, {text: input, completed: false, id: Math.random()*1000 },
-                 ]);
-                 setInput("");
-    };
+  const formSubmit = (e) => {
+    e.preventDefault();
+    setTodos([...todos, { text: input, completed: false, id: uuid() }]);
+    setInput("");
+  };
 
-    const filterHandler =(e) =>{
-        setFilter(e.target.value);
-    }
+  const filterHandler = (e) => {
+    setFilter(e.target.value);
+  };
 
-    return(
-     <form>
-       <input 
-       type="text" 
-       name="name" 
-       placeholder="Add Task"
-       value={input} 
-       required
-       onChange = {inputHandler}
-        />
-       <button onClick={formSubmit} className="btn-add" type="submit">Add new task</button>
+  return (
+    <form>
+      <input
+        type="text"
+        name="name"
+        placeholder="Add Task"
+        value={input}
+        onChange={inputHandler}
+      />
+      <button onClick={formSubmit} disabled={!input} type="submit">
+        Add new task
+      </button>
 
-       <div className="filter">
-            <select onChange={filterHandler}>
-                <option value="all">All</option>
-                <option value="completed">Done</option>
-                <option value="uncompleted">Pending</option>
-            </select>
-        </div>
-
-     </form>       
-    );
+      <div className="filter">
+        <select onChange={filterHandler}>
+          <option value="all">All</option>
+          <option value="completed">Done</option>
+          <option value="uncompleted">Pending</option>
+        </select>
+      </div>
+    </form>
+  );
 };
 
 export default Form;
-
-
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,32 +1,18 @@
-import React from "react";
-
-const Todo =({text, todo, setTodos, todos})=>{
-
-    const onDeleter = ()=>{
-        setTodos(todos.filter((el) =>el.id !==todo.id));
-    }
-
-    const onComplete = () =>{
-        setTodos(
-            todos.map((item) => {
-                if(item.id === todo.id){
-                    return{
-                        ...item, completed: !item.completed
-                    } 
-                }
-                return item;
-            })
-        )
-    }
-
-    return(
-     <div className="tasks">
-        <li className={`task-item ${todo.completed ? "completed" : ""}`}>{text}</li>
-        <button className="btn-completed" onClick={onComplete}>Completed</button>
-        <button className="btn-delete" onClick={onDeleter}>Delete</button>
-     </div>
-       
-    );
-}
+const Todo = ({ text, todo, onComplete, onDeleter }) => {
+  return (
+    <li className="tasks">
+      <li className={`task-item ${todo.completed ? "completed" : ""}`}>
+        {text}
+      </li>
+      <button className="btn-completed" onClick={onComplete}>
+        Completed
+      </button>
+      <button className="btn-delete" onClick={onDeleter}>
+        Delete
+      </button>
+    </li>
+  );
+  
+};
 
 export default Todo;
