07728e4e26ae6996817fa019366f88e1b24aa3c7
zbednarz <zbigniew.bednarz@icloud.com>
2022-10-10 14:50:37 +0200

    errors in console log fixed

--- src/App.js
+++ src/App.js
@@ -64,4 +64,3 @@ export default function App() {
   );
 }
 
-//export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -5,11 +5,10 @@ import { NewTask } from "../styles/MyButton";
 import { MyBox } from "../styles/MyForm";
 import MySelect from "../styles/MyForm";
 import { MenuItem } from "@mui/material";
-
 import React from "react";
 import { useForm, Controller } from "react-hook-form";
 
-const Form = ({ input, setInput, todos, setTodos, setFilter }) => {
+const Form = ({ input, setInput, todos, setTodos, setFilter, filter }) => {
   const inputHandler = (e) => {
     setInput(e.target.value);
   };
@@ -25,10 +24,9 @@ const Form = ({ input, setInput, todos, setTodos, setFilter }) => {
   };
 
   const {
-    register,
     handleSubmit,
     control,
-    formState: { errors },
+    // formState: { errors },
   } = useForm();
   const onSubmit = (data) => console.log(data);
 
@@ -39,52 +37,54 @@ const Form = ({ input, setInput, todos, setTodos, setFilter }) => {
   ];
 
   return (
-    <form>
-      <MyBox noValidate autoComplete="off">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Controller
-            name="task"
-            control={control}
-            rules={{ required: true }}
-            render={({ field }) => (
-              <MyInput
-                {...field}
-                label={
-                  <Typography
-                    variant="headline"
-                    component="h2"
-                    color="secondary"
-                  >
-                    {" "}
-                    Add Task{" "}
-                  </Typography>
-                }
-                onChange={inputHandler}
-                placeholder="Task"
-                type="text"
-                value={input}
-              />
-            )}
-          />
-          <NewTask onClick={formSubmit} type="submit" variant="contained">
-            Add new task
-          </NewTask>
-          <Controller
-            name="options"
-            control={control}
-            render={({ field }) => (
-              <MySelect onChange={filterHandler} label="Filter" {...field}>
-                {options.map((option, index) => (
-                  <MenuItem key={index} value={option.value}>
-                    {option.text}
-                  </MenuItem>
-                ))}
-              </MySelect>
-            )}
-          />
-        </form>
-      </MyBox>
-    </form>
+    <MyBox noValidate autoComplete="off">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="task"
+          control={control}
+          rules={{ required: true }}
+          render={({ field }) => (
+            <MyInput
+              {...field}
+              label={
+                <Typography variant="headline" component="h2" color="secondary">
+                  {" "}
+                  Add Task{" "}
+                </Typography>
+              }
+              onChange={inputHandler}
+              placeholder="Task"
+              type="text"
+              value={input}
+            />
+          )}
+        />
+        <NewTask onClick={formSubmit} variant="contained">
+          Add new task
+        </NewTask>
+        <Controller
+          name="options"
+          control={control}
+          render={({ field }) => (
+            <MySelect
+              value={filter}
+              defaultValue="all"
+              label="Filter"
+              {...field} 
+              onChange={filterHandler}
+            >
+              {options.map((option, index) => (
+                <MenuItem 
+                key={index} 
+                value={option.value || ''}>
+                {option.text}
+                </MenuItem>
+              ))}
+            </MySelect>
+          )}
+        />
+      </form>
+    </MyBox>
   );
 };
 
